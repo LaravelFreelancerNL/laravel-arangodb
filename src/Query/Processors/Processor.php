c8f97c702d29c8a0fa5949af175ab3fcd4c60882
bas <bas@devechtschool.nl>
2018-12-02 10:22:28 +0100

    Processor class in preparation of the query builder

---
+++ src/Query/Processors/Processor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaravelFreelancerNL\Aranguent\Query\Processors;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Processors\Processor as IlluminateProcessor;
+
+class Processor extends IlluminateProcessor
+{
+}
