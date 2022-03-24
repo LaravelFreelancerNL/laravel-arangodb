af4ca548aef4a7be74a9794a679664d685850306
Bas <info@laravel-freelancer.nl>
2022-03-24 20:52:01 +0100

    Added support for json document paths in selects.
    Fixed addSelect overwriting the additional select.

--- src/Query/Concerns/CompilesColumns.php
+++ src/Query/Concerns/CompilesColumns.php
@@ -1,8 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace LaravelFreelancerNL\Aranguent\Query\Concerns;
 
+use Exception;
+use Illuminate\Support\Arr;
 use LaravelFreelancerNL\Aranguent\Query\Builder;
+use LaravelFreelancerNL\FluentAQL\Expressions\FunctionExpression;
+use LaravelFreelancerNL\FluentAQL\QueryBuilder;
 
 trait CompilesColumns
 {
@@ -10,20 +16,21 @@ trait CompilesColumns
      * Compile the "RETURN" portion of the query.
      *
      * @param Builder $builder
-     * @param array   $columns
+     * @param array $columns
      *
      * @return Builder
+     * @throws Exception
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
      */
     protected function compileColumns(Builder $builder, array $columns): Builder
     {
         $returnDocs = [];
         $returnAttributes = [];
-        $values = [];
 
         // Prepare columns
-        foreach ($columns as $column) {
+        foreach ($columns as $key => $column) {
             // Extract rows
-            if (substr($column, strlen($column) - 2)  === '.*') {
+            if (is_string($column) && substr($column, strlen($column) - 2)  === '.*') {
                 $table = substr($column, 0, strlen($column) - 2);
                 $returnDocs[] = $this->getTableAlias($table);
 
@@ -32,23 +39,117 @@ trait CompilesColumns
 
             // Extract groups
             if (is_array($builder->groups) && in_array($column, $builder->groups)) {
-                $returnAttributes[] = $column;
+                $returnAttributes[$key] = $column;
 
                 continue;
             }
 
-            if ($column != null && $column != '*') {
-                [$column, $alias] = $this->extractAlias($column);
+            if (is_string($column) && $column != null && $column != '*') {
+                [$column, $alias] = $this->normalizeStringColumn($builder, $key, $column);
+
+                if (isset($returnAttributes[$alias]) && is_array($column)) {
+                    $returnAttributes[$alias] = array_merge_recursive(
+                        $returnAttributes[$alias],
+                        $this->normalizeColumn($builder, $column)
+                    );
+                    continue;
+                }
 
                 $returnAttributes[$alias] = $this->normalizeColumn($builder, $column);
             }
         }
+
         $values = $this->determineReturnValues($builder, $returnAttributes, $returnDocs);
 
         $builder->aqb = $builder->aqb->return($values, (bool) $builder->distinct);
 
         return $builder;
     }
+
+    /**
+     * @throws Exception
+     */
+    protected function normalizeColumn(Builder $builder, mixed $column, string $table = null): mixed
+    {
+        if ($column instanceof QueryBuilder || $column instanceof FunctionExpression) {
+            return $column;
+        }
+
+        $column = $this->convertColumnId($column);
+
+        if (is_array($builder->groups) && in_array($column, $builder->groups)) {
+            return $column;
+        }
+
+        if (is_array($column)) {
+            foreach ($column as $key => $value) {
+                $column[$key] = $this->normalizeColumn($builder, $value, $table);
+            }
+            return $column;
+        }
+
+        if (key_exists($column, $builder->variables)) {
+            return $column;
+        }
+
+        //We check for an existing alias to determine of the first reference is a table.
+        // In which case we replace it with the alias.
+        return $this->normalizeColumnReferences($builder, $column, $table);
+    }
+
+    /**
+     * @param array<mixed> $column
+     * @return array<mixed>
+     * @throws Exception
+     */
+    protected function normalizeStringColumn(Builder $builder, int|string $key, mixed $column): array
+    {
+        [$column, $alias] = $this->extractAlias($column, $key);
+
+        if (! isDotString($alias)) {
+            $column = $this->normalizeColumn($builder, $column);
+
+            return [$column, $alias];
+        }
+
+        $column = Arr::undot([$column => $column]);
+        $alias = array_key_first($column);
+        $column = $column[$alias];
+        return [$column, $alias];
+    }
+
+
+    /**
+     * @param Builder $builder
+     * @param string $column
+     * @param string|null $table
+     * @return string
+     */
+    protected function normalizeColumnReferences(Builder $builder, string $column, string $table = null): string
+    {
+        if ($table == null) {
+            $table = $builder->from;
+        }
+
+        // Replace SQL JSON arrow for AQL dot
+        $column = str_replace('->', '.', $column);
+
+        $references = explode('.', $column);
+
+
+        $tableAlias = $this->getTableAlias($references[0]);
+        if (isset($tableAlias)) {
+            $references[0] = $tableAlias;
+        }
+
+        if ($tableAlias === null && $table != null && ! $this->isTableAlias($references[0])) {
+            $tableAlias = $this->generateTableAlias($table);
+            array_unshift($references, $tableAlias);
+        }
+
+        return implode('.', $references);
+    }
+
 
     protected function determineReturnValues($builder, $returnAttributes = [], $returnDocs = [])
     {
---
+++ tests/Query/SelectTest.php
@@ -0,0 +1,116 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use LaravelFreelancerNL\Aranguent\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+uses(
+    TestCase::class,
+    DatabaseTransactions::class
+);
+
+test('basic select', function () {
+    $results = DB::table('characters')->select()->get();
+
+    expect($results)->toHaveCount(43);
+    expect(count((array)$results[0]))->toBe(9);
+});
+
+test('basic select with specific column', function () {
+    $results = DB::table('characters')->select('name')->get();
+
+    expect($results[0])->toBe('Ned');
+});
+
+test('basic select with specific columns', function () {
+    $results = DB::table('characters')->select(['name', 'surname', 'age'])->limit(1)->get();
+
+    expect($results[0])->toHaveProperties(['name', 'surname', 'age']);
+});
+
+test('select nested data through alias', function () {
+    $house = DB::table('houses')->select([
+        'name',
+        'en.description as description',
+    ])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('description');
+});
+
+test('select nested data without alias', function () {
+    $house = DB::table('houses')->select([
+        'name',
+        'en.description',
+    ])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('en');
+});
+
+test('select nested data through string key', function () {
+    $house = DB::table('houses')->select([
+        'name',
+        'description' => 'en.description',
+    ])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('description');
+});
+
+test('select nested data with embedded objects though multiple paths', function () {
+    $house = DB::table('houses')->select([
+        'name',
+        'en.description',
+        'en.words',
+    ])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('en');
+    expect((array)$house->en)->toHaveCount(2);
+});
+
+test('select nested data with multilevel embedded objects though multiple paths', function () {
+    $house = DB::table('houses')->select([
+        'en.summary.short',
+        'en.summary.long',
+        'en.words',
+        'name',
+    ])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('en');
+    expect((array)$house->en)->toHaveCount(2);
+    expect((array)$house->en->summary)->toHaveCount(2);
+});
+
+test('addSelect', function () {
+    $house = DB::table('houses')->select('en')->addSelect('name')->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('en');
+});
+
+test('addSelect multiple', function () {
+    $house = DB::table('houses')->select('en.description')->addSelect(['name', 'en.words'])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('name');
+    expect($house)->toHaveProperty('en');
+    expect((array)$house->en)->toHaveCount(2);
+});
+
+test('addSelect with alias', function () {
+    $house = DB::table('houses')->select('en.description')->addSelect(['house_name' => 'name', 'en.words'])->limit(1)->first();
+
+    expect((array)$house)->toHaveCount(2);
+    expect($house)->toHaveProperty('house_name');
+    expect($house)->toHaveProperty('en');
+    expect((array)$house->en)->toHaveCount(2);
+});
