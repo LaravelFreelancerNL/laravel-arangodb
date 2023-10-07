cb6a73e61741e49f9a44bfa85c09ed087a7773f0
Bas <info@laravel-freelancer.nl>
2023-10-07 17:48:56 +0200

    converted limit clauses

---
+++ tests/Query/LimitTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\DB;
+use LaravelFreelancerNL\Aranguent\Testing\DatabaseTransactions;
+
+uses(
+    \Tests\TestCase::class,
+    DatabaseTransactions::class
+);
+
+test('limit', function () {
+    $allCharacters = DB::table('characters')->get();
+    $result = DB::table('characters')->limit(15)->get();
+
+    expect($allCharacters->count())->toEqual(43);
+    expect($result->count())->toEqual(15);
+    expect($result->first())->toEqual($allCharacters->first());
+});
+
+test('limit with offset', function () {
+    $allCharacters = DB::table('characters')->get();
+    $result = DB::table('characters')->limit(15)->offset(5)->get();
+
+    expect($allCharacters->count())->toEqual(43);
+    expect($result->count())->toEqual(15);
+    expect($result->first())->toEqual($allCharacters[5]);
+});
+
+test('take', function () {
+    $allCharacters = DB::table('characters')->get();
+    $result = DB::table('characters')->take(15)->get();
+
+    expect($allCharacters->count())->toEqual(43);
+    expect($result->count())->toEqual(15);
+    expect($result->first())->toEqual($allCharacters->first());
+});
+
+test('skip & take', function () {
+    $allCharacters = DB::table('characters')->get();
+    $result = DB::table('characters')->skip(5)->take(15)->get();
+
+    expect($allCharacters->count())->toEqual(43);
+    expect($result->count())->toEqual(15);
+    expect($result->first())->toEqual($allCharacters[5]);
+});
+
