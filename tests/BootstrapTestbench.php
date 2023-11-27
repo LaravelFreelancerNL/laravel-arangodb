8b2de8473df6317dad252ec07507704573e69307
Bas <info@laravel-freelancer.nl>
2023-11-27 16:43:08 +0100

    testbench bootstrap test

--- tests/BootstrapTestbench.php
+++ tests/BootstrapTestbench.php
@@ -3,9 +3,6 @@
 namespace Tests;
 
 use Illuminate\Contracts\Foundation\Application;
-use Spatie\Ray\Settings\Settings;
-
-use function Orchestra\Testbench\after_resolving;
 
 /**
  * @internal
@@ -24,21 +21,16 @@ class BootstrapTestbench
      */
     public function bootstrap(Application $app): void
     {
-        after_resolving($app, Settings::class, static function ($settings, $app) {
-            dump('BootstrapTestbench is used!');
-            die;
+        $config = $app->make('config');
 
-            $config = $app->make('config');
-
-            $config->set('database.connections.arangodb', [
-                'name'                                      => 'arangodb',
-                'driver'                                    => 'arangodb',
-                'endpoint'                                  => env('DB_ENDPOINT', 'http://localhost:8529'),
-                'username'                                  => env('DB_USERNAME', 'root'),
-                'password'                                  => env('DB_PASSWORD', null),
-                'database'                                  => env('DB_DATABASE', 'aranguent__test'),
-            ]);
-            $config->set('database.default', 'arangodb');
-        });
+        $config->set('database.connections.arangodb', [
+            'name'                                      => 'arangodb',
+            'driver'                                    => 'arangodb',
+            'endpoint'                                  => env('DB_ENDPOINT', 'http://localhost:8529'),
+            'username'                                  => env('DB_USERNAME', 'root'),
+            'password'                                  => env('DB_PASSWORD', null),
+            'database'                                  => env('DB_DATABASE', 'aranguent__test'),
+        ]);
+        $config->set('database.default', 'arangodb');
     }
 }
