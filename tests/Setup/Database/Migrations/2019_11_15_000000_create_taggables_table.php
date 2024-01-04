7ff5a51313aa5fdb86b69f580cc03c93c714f16c
Bas <info@laravel-freelancer.nl>
2024-01-04 15:57:05 +0100

    Cleared old phpcs message
    Removed fromAqb docs for now

--- tests/Setup/Database/Migrations/2019_11_15_000000_create_taggables_table.php
+++ tests/Setup/Database/Migrations/2019_11_15_000000_create_taggables_table.php
@@ -1,6 +1,5 @@
 <?php
 
-// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
 use Illuminate\Database\Migrations\Migration;
 use LaravelFreelancerNL\Aranguent\Facades\Schema;
 use LaravelFreelancerNL\Aranguent\Schema\Blueprint;
