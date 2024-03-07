d59e6e8429490a7558dab6e067ecc82c49e3ed05
Bas <info@laravel-freelancer.nl>
2024-03-07 18:07:18 +0100

    Added orchestra/testbench migration path.

--- bin/qa.sh
+++ bin/qa.sh
@@ -6,7 +6,7 @@ echo "Run PHPMD"
 ./vendor/bin/phpmd src/ text phpmd-ruleset.xml
 
 echo "Test package from within phpunit"
-./vendor/bin/testbench migrate:fresh --path=tests/Setup/Database/Migrations --realpath --seed
+./vendor/bin/testbench migrate:fresh --path=tests/Setup/Database/Migrations --path=vendor/orchestra/testbench-core/laravel/migrations/ --realpath --seed
 ./vendor/bin/testbench package:test --coverage --min=80 tests
 
 echo "Run PHPStan"
